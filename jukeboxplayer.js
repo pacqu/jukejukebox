0249debac3c2de463bc34939a198ec04dd2ed43b
Noam Sohn <noamsohn@N-MacBook-Air.local>
2018-06-01 11:03:46 -0400

    commented functionality

--- jukeboxplayer.js
+++ jukeboxplayer.js
@@ -1,3 +1,17 @@
-function play_song(){ //argument is song?
+/*
+Updates:
+* album artwork
+* artist name
+* album title
+* song title
+* timing of song ? is this continious?
+* begins playing
 
+*/
+function changeSong(songOrder){
+
+  //call play song
+}
+function play_song(){ //argument is song?
+  document.getElementById('songID').play();
 }
