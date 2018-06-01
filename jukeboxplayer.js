b1303baf0a08404c9d43a4af41bc0a7407afe6f9
Noam Sohn <noamsohn@N-MacBook-Air.local>
2018-06-01 11:11:00 -0400

    accident in wrong branch

---
+++ jukeboxplayer.js
@@ -0,0 +1,17 @@
+/*
+Updates:
+* album artwork
+* artist name
+* album title
+* song title
+* timing of song ? is this continious
+* begins playing
+
+*/
+function changeSong(songOrder){
+
+  //call play song
+}
+function play_song(){ //argument is song?
+  document.getElementById('songID').play();
+}
