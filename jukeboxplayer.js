ebd7f4d498853ebe5b98dd9d660deca3039b1df1
Noam Sohn <noamsohn@N-MacBook-Air.local>
2018-06-01 11:47:57 -0400

    readdding file

---
+++ jukeboxplayer.js
@@ -0,0 +1,27 @@
+/*
+Updates:
+* artist name
+* album title
+* album artwork
+* song title
+* timing of song ? is this continious?
+* begins playing
+
+*/
+function changeSong(songOrder){
+  document.getElementById('active-song-artist').innerHTML = songOrder.songArtist;
+  document.getElementById('active-song-album').innerHTML = songOrder.songAlbum;
+  document.getElementById('active-song-artwork').innerHTML = songOrder.songArtFile;
+  document.getElementById('active-song-title').innerHTML = songOrder.songTitle;
+  play_song();
+}
+
+function play_song(){
+  document.getElementById('play-button').innerHTML = pause.png
+  document.getElementById('songID').play();
+}
+
+function pause_song(){
+  document.getElementById('play-button').innerHTML = play.jpg
+  document.getElementById('songID').pause();
+}
